7d29e6224e11e1588cb9ccc87eb9e1e872eb6d67
rinxiaoxiao <33726100+rinxiaoxiao@users.noreply.github.com>
2026-03-18 13:44:06 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -32,11 +32,11 @@ const CONFIG = {
 
   HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
   HEXO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  HEXO_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
+  HEXO_ARTICLE_NOT_BY_AI: true, // 显示非AI写作
   HEXO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
-  HEXO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
-  HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
+  HEXO_WIDGET_LATEST_POSTS: false, // 显示最新文章卡
+  HEXO_WIDGET_ANALYTICS: true, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
   HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
   HEXO_WIDGET_DARK_MODE: true, // 夜间模式
